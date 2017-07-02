31effdcf16a3a60b7053a6ac3eb0b75882b29d19
visonforcoding <caowenpeng1990@126.com>
2017-07-02 12:05:59 +0800

    update match_douban

--- api/script/match_douban.py
+++ api/script/match_douban.py
@@ -33,26 +33,27 @@ total_counts = movie_collection.find().count()
 
 random_int = random.randint(0, total_counts - 1)
 
-random_doc = movie_collection.find().limit(1).skip(random_int).next()
-movie_title = random_doc['title']
-movie_title = movie_title.encode('utf-8')
-logger.info('%s被拿去豆瓣匹配' % movie_title)
-api_url = 'https://api.douban.com/v2/movie/search?q=' + movie_title
+random_doc = movie_collection.find(
+    {'db_math': {'$ne': True}}).limit(1).skip(random_int).next()
+if random_doc:
+    movie_title = random_doc['title']
+    movie_title = movie_title.encode('utf-8')
+    logger.info('%s被拿去豆瓣匹配' % movie_title)
+    api_url = 'https://api.douban.com/v2/movie/search?q=' + movie_title
 
-res = requests.get(api_url)
-res_obj = json.loads(res.content)
-if res_obj['total'] > 1 or res_obj['total'] == 1:
-    douban_subject = res_obj['subjects'][0]
-    update_doc = {
-        'db_match': True,
-        'db_subject': douban_subject
-    }
-    movie_collection.update_one({'_id': random_doc['_id']}, {
-        '$set': update_doc
-    })
-    logger.info('%s匹配成功' % str(movie_title))
-elif res_obj['total'] == 0:
-    logger.info('%s匹配失败' % str(movie_title))
-
+    res = requests.get(api_url)
+    res_obj = json.loads(res.content)
+    if res_obj['total'] > 1 or res_obj['total'] == 1:
+        douban_subject = res_obj['subjects'][0]
+        update_doc = {
+            'db_match': True,
+            'db_subject': douban_subject
+        }
+        movie_collection.update_one({'_id': random_doc['_id']}, {
+            '$set': update_doc
+        })
+        logger.info('%s匹配成功' % str(movie_title))
+    elif res_obj['total'] == 0:
+        logger.info('%s匹配失败' % str(movie_title))
 else:
-    logger.info('%s记录过多，跳过更新' % str(movie_title))
+    logger.info('无需匹配记录')
